b9bbaaa63a034d1e9982594aa0735d91acc2be15
Dhanush <dhanushxenocrate@gmail.com>
2024-05-13 13:24:51 +0000

    Minor checks

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 function LandingPage() {
   return (
     <div className="landing-page">
-      <h1>Welcome to Dhanush N Code Editor</h1>
-      <Link to="/editor">Editor</Link>
+      <h1>Welcome</h1>
+      <Link to="/editor"> Click Here To Navigate To Editor </Link>
     </div>
   );
 }
