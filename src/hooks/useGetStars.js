f5f559a226c14a8f955b15b5f9c78d75c9778417
skyDe <sky_de1991@outlook.com>
2024-05-18 00:51:46 -0800

    gitHub stars added to project

---
+++ src/hooks/useGetStars.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+const initialFetchState = {
+  isLoading: false,
+  isError: false,
+  errorMessage: "",
+};
+
+export const useGetStars = () => {
+  const [fetchState, setFetchState] = useState(initialFetchState);
+  const [repositoryStarts, setRepositoryStars] = useState(0);
+
+  const getStars = async () => {
+    try {
+      setFetchState({ ...initialFetchState, isLoading: true });
+      const res = await fetch(
+        "https://api.github.com/repos/DhanushNehru/CustomCodeEditor"
+      );
+      console.log(res.status);
+      if (res.status === 200) {
+        const result = await res.json();
+        setRepositoryStars(result.stargazers_count);
+        setFetchState(initialFetchState);
+      } else
+        throw Error("Error happend in getting repository stars, retry again!.");
+    } catch (error) {
+      setFetchState({
+        ...initialFetchState,
+        isError: true,
+        errorMessage: error.message,
+      });
+    }
+  };
+
+  useEffect(() => {
+    getStars();
+  }, []);
+
+  return { fetchState, repositoryStarts, getStars };
+};
