e1741a0f2eaa443c358696ab43d765b9814c2143
Chaitanya Narhare <chetannarhare111@gmail.com>
2024-05-25 20:07:19 +0530

    Implented eslint feature in project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import EditorComponent from './pages/EditorComponent';
-import './components/css/App.css'
-import { ThemeProvider } from '@mui/material';
-import theme from './theme';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import EditorComponent from "./pages/EditorComponent";
+import "./components/css/App.css"
+import { ThemeProvider } from "@mui/material";
+import theme from "./theme";
 
 function App() {
   return (
--- src/components/js/sitemapGenerator.js
+++ src/components/js/sitemapGenerator.js
@@ -1,16 +1,16 @@
 require("babel-register")({
-    presets: ["es2015", "react"]
+  presets: ["es2015", "react"]
 });
 
 const router = require("./sitemapRoutes").default;
 const Sitemap = require("@snaddyvitch-dispenser/react-router-sitemap").default;
 
 function generateSitemap() {
-        return (
-            new Sitemap(router)
-                    .build("https://custom-code-editor.vercel.app")
-                    .save("./public/sitemap.xml")
-        );
+  return (
+    new Sitemap(router)
+      .build("https://custom-code-editor.vercel.app")
+      .save("./public/sitemap.xml")
+  );
 }
 
 generateSitemap();
