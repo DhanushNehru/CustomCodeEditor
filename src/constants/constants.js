3beda9010c95f853cd634b0a7477f92f3985d48c
Chaitanya Narhare <chetannarhare111@gmail.com>
2024-06-01 13:51:22 +0530

    Added Java language and moved all svg logo to separate file. Also imported new cpp and java logo

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,8 @@
+import CppLogo from "../Images/CppLogo"
+import JavaLogo from "../Images/JavaLogo"
+import JavascriptLogo from "../Images/JavaScriptLogo"
+import PythonLogo from "../Images/PythonLogo"
+
 export const judge0SubmitUrl =
   process.env.JUDGE0_SUMBISSION_URL || process.env.REACT_APP_RAPID_API_URL
 export const rapidApiHost = process.env.REACT_APP_RAPID_API_HOST
@@ -6,21 +11,31 @@ export const rapidApiKey = process.env.REACT_APP_RAPID_API_KEY
 export const LANGUAGE_ID_FOR_JAVASCRIPT = 63
 export const LANGUAGE_ID_FOR_PYTHON3 = 71
 export const LANGUAGE_ID_FOR_CPP = 76
+export const LANGUAGE_ID_FOR_JAVA = 62;
 
 export const LANGUAGES = [
   {
     ID: LANGUAGE_ID_FOR_JAVASCRIPT,
     NAME: "Javascript",
     DEFAULT_LANGUAGE: "javascript",
+    LOGO: <JavascriptLogo/>
   },
   {
     ID: LANGUAGE_ID_FOR_PYTHON3,
     NAME: "Python3",
     DEFAULT_LANGUAGE: "python",
+    LOGO: <PythonLogo/>
   },
   {
     ID: LANGUAGE_ID_FOR_CPP,
     NAME: "C++",
-    DEFAULT_LANGUAGE: "C++(Clang 7.0.1)"
+    DEFAULT_LANGUAGE: "C++(Clang 7.0.1)",
+    LOGO: <CppLogo/>
+  },
+  {
+    ID: LANGUAGE_ID_FOR_JAVA,
+    NAME: "Java",
+    DEFAULT_LANGUAGE: "java",
+    LOGO: <JavaLogo/>
   },
 ]; 
