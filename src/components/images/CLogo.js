d4a58b0cebed722663631f859c6420b20817952a
gauravPanditr <gauravpandit7856@gmail.com>
2024-10-04 20:56:17 +0530

    Add C Language

---
+++ src/components/images/CLogo.js
@@ -0,0 +1,76 @@
+export default function CLogo({ width = 40, height = 40 }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      x="0px"
+      y="0px"
+      width={width}
+      height={height}
+      viewBox="0 0 48 48"
+    >
+      <linearGradient
+        id="Ey3AfYdg0JtJGx7I73Eu7a_TpULddJc4gTh_gr1"
+        x1="5"
+        x2="43"
+        y1="24"
+        y2="24"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset=".002" stopColor="#427fdb"></stop>
+        <stop offset=".397" stopColor="#2668cb"></stop>
+        <stop offset=".763" stopColor="#1358bf"></stop>
+        <stop offset="1" stopColor="#0c52bb"></stop>
+      </linearGradient>
+      <path
+        fill="url(#Ey3AfYdg0JtJGx7I73Eu7a_TpULddJc4gTh_gr1)"
+        fillRule="evenodd"
+        d="M22.903,3.286c0.679-0.381,1.515-0.381,2.193,0	c3.355,1.883,13.451,7.551,16.807,9.434C42.582,13.1,43,13.804,43,14.566c0,3.766,0,15.101,0,18.867	c0,0.762-0.418,1.466-1.097,1.847c-3.355,1.883-13.451,7.551-16.807,9.434c-0.679,0.381-1.515,0.381-2.193,0	c-3.355-1.883-13.451-7.551-16.807-9.434C5.418,34.899,5,34.196,5,33.434c0-3.766,0-15.101,0-18.867	c0-0.762,0.418-1.466,1.097-1.847C9.451,10.837,19.549,5.169,22.903,3.286z"
+        clipRule="evenodd"
+      ></path>
+      <linearGradient
+        id="Ey3AfYdg0JtJGx7I73Eu7b_TpULddJc4gTh_gr2"
+        x1="5"
+        x2="42.487"
+        y1="18.702"
+        y2="18.702"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0" stopColor="#32bdef"></stop>
+        <stop offset="1" stopColor="#1ea2e4"></stop>
+      </linearGradient>
+      <path
+        fill="url(#Ey3AfYdg0JtJGx7I73Eu7b_TpULddJc4gTh_gr2)"
+        fillRule="evenodd"
+        d="M5.304,34.404C5.038,34.048,5,33.71,5,33.255c0-3.744,0-15.014,0-18.759c0-0.758,0.417-1.458,1.094-1.836	c3.343-1.872,13.405-7.507,16.748-9.38c0.677-0.379,1.594-0.371,2.271,0.008c3.343,1.872,13.371,7.459,16.714,9.331	c0.27,0.152,0.476,0.335,0.66,0.576L5.304,34.404z"
+        clipRule="evenodd"
+      ></path>
+      <path
+        fill="#fff"
+        fillRule="evenodd"
+        d="M24,10c7.727,0,14,6.273,14,14s-6.273,14-14,14s-14-6.273-14-14	S16.273,10,24,10z M24,17c3.863,0,7,3.136,7,7c0,3.863-3.137,7-7,7s-7-3.137-7-7C17,20.136,20.136,17,24,17z"
+        clipRule="evenodd"
+      ></path>
+      <linearGradient
+        id="Ey3AfYdg0JtJGx7I73Eu7c_TpULddJc4gTh_gr3"
+        x1="23.593"
+        x2="43"
+        y1="23.852"
+        y2="23.852"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0" stopColor="#2aa4f4"></stop>
+        <stop offset="1" stopColor="#007ad9"></stop>
+      </linearGradient>
+      <path
+        fill="url(#Ey3AfYdg0JtJGx7I73Eu7c_TpULddJc4gTh_gr3)"
+        fillRule="evenodd"
+        d="M42.485,13.205c0.516,0.483,0.506,1.211,0.506,1.784c0,3.795-0.032,14.589,0.009,18.384c0.004,0.396-0.127,0.813-0.323,1.127	L23.593,24L42.485,13.205z"
+        clipRule="evenodd"
+      ></path>
+         
+          
+         
+    </svg>
+  );
+}
+        
