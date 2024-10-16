24cd21388a82ee3c83326e875a36a6e599e7a671
Incharajayaram <incharajayaram2020@gmail.com>
2024-10-16 20:35:46 +0530

    changes in dependencies, ui and improved readme part of firebase auth

--- src/hooks/useGetStars.js
+++ src/hooks/useGetStars.js
@@ -21,7 +21,7 @@ export const useGetStars = () => {
         setRepositoryStars(result.stargazers_count);
         setFetchState(initialFetchState);
       } else
-        throw Error("Error happend in getting repository stars, retry again!.");
+        throw Error("Error happened in getting repository stars, retry again!.");
     } catch (error) {
       setFetchState({
         ...initialFetchState,
